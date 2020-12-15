e1907afff13633b63a339a48bf80bbd73ff0d202
Christopher Tan <ctan20@stuy.edu>
2020-12-15 14:31:27 -0500

    Added preliminary test cases and finished method

--- Sorts.java
+++ Sorts.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class Sorts{
     public static void bubbleSort(int[] data) {
-        
+        boolean no_swap = false;
+        for (int i = 0; i < data.length && !no_swap; i++) {
+            System.out.println(Arrays.toString(data));
+            no_swap = true;
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] > data[j+1]) {
+                    int temp = data[j];
+                    data[j] = data[j+1];
+                    data[j+1] = temp;
+                    no_swap = false;
+                }
+            }
+        }
     }
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
 public class Tester {
     public static void main(String[] args) {
-        
+        int[] a = {5,1,12,-5,16};
+        Sorts.bubbleSort(a);
+        System.out.println(Arrays.toString(a));
+        int[] b = {2,3,4,5,1};
+        Sorts.bubbleSort(b);
+        System.out.println(Arrays.toString(b));
+        int[] c = {6,1,2,3,4,5};
+        Sorts.bubbleSort(c);
+        System.out.println(Arrays.toString(c));
     }
 }
